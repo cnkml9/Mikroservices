afeea8b7df80d0eef4f44c517507e14b5646ea59
Kamil Can <kmlcnebay@gmail.com>
2023-10-21 15:55:51 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Services/ContactService.cs
@@ -0,0 +1,24 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.API.Services
+{
+    public class ContactService : IContactService
+    {
+        public ContactDTO getContactById(int id)
+        {
+            return new ContactDTO()
+            {
+                Id = id,
+                FirstName = "Kamil",
+                LastName = "Can",
+
+            };
+        }
+    }
+}
